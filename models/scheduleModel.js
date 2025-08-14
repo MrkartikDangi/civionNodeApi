9b9be2f7c886ba64ae15d474c1664c6405fc9fec
MrkartikDangi <kpdangi660@gmail.com>
2025-08-15 00:46:29 +0530

    changes

--- models/scheduleModel.js
+++ models/scheduleModel.js
@@ -6,9 +6,9 @@ Schedule.getScheduleData = (postData) => {
   return new Promise((resolve, reject) => {
     let whereCondition = ``
     if (postData.filter && postData.filter.schedule_id) {
-      whereCondition += `And id = ${postData.filter.schedule_id}`
+      whereCondition += `And ks.id = ${postData.filter.schedule_id}`
     }
-    let query = `SELECT kps_schedules.*, IFNULL(DATE_FORMAT(created_at, '%Y-%m-%d %H:%i:%s'), '') AS created_at,IFNULL(DATE_FORMAT(updated_at, '%Y-%m-%d %H:%i:%s'), '') AS updated_at,IFNULL(CONCAT('${process.env.Base_Url}',kps_schedules.folder_name,'/', kps_schedules.pdfUrl), '') as pdfUrl FROM kps_schedules WHERE 1 = 1 ${whereCondition}`
+    let query = `SELECT ks.*, IFNULL(DATE_FORMAT(ks.created_at, '%Y-%m-%d %H:%i:%s'), '') AS created_at, IFNULL(DATE_FORMAT(ks.updated_at, '%Y-%m-%d %H:%i:%s'), '') AS updated_at, IFNULL(CONCAT('${process.env.Base_Url}', ks.folder_name, '/', ks.pdfUrl), '') AS pdfUrl, IFNULL(inv.description, '') AS invoiceDescription FROM kps_schedules ks LEFT JOIN (SELECT ki.schedule_id, ki.description FROM kps_invoice ki INNER JOIN (SELECT schedule_id, MAX(created_at) AS max_created FROM kps_invoice GROUP BY schedule_id) latest ON latest.schedule_id = ki.schedule_id AND latest.max_created = ki.created_at) inv ON ks.id = inv.schedule_id WHERE 1 = 1 ${whereCondition}`
     let values = []
     db.query(query, values, (err, res) => {
       if (err) {
